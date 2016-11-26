87c3fe91fdf57b1ff4c4db0ab85487dcba0a5b8e
guest-Rp1thi <guest-Rp1thi@DINSL009-100088>
2016-11-25 22:12:14 -0200

    ESTE COMANDO FOI PROIBIDO EM MAIS DE 42 PAÍSES
    -Bruno

--- ISS_Farmacia/src/main/java/iss_trab_farmacia/PessoaFisica.java
+++ ISS_Farmacia/src/main/java/iss_trab_farmacia/PessoaFisica.java
@@ -5,17 +5,15 @@
  */
 package iss_trab_farmacia;
 
-import iss_trab_farmacia.control.Pessoa;
-import iss_trab_farmacia.entity.PessoaFisica;
 import org.mongodb.morphia.Datastore;
 
 /**
  *
- * @author guest-1HRmBh
+ * @author guest-Rp1thi
  */
-public class PessoaFisica extends javax.swing.JFrame {
+public class PessoaFisica extends javax.swing.JPanel {
 
-    private final Pessoa pessoa;
+    //private final Pessoa pessoa;
     
     /**
      * Creates new form FarmaciaInterface
@@ -23,10 +21,8 @@ public class PessoaFisica extends javax.swing.JFrame {
      */
     public PessoaFisica(Datastore ds) {
         initComponents();
-        btGrupoTipoPessoa.add(btPessoaFisica);
-        btGrupoTipoPessoa.add(btPessoaJuridica);
         
-        this.pessoa = new Pessoa(ds);
+//      this.pessoa = new Pessoa(ds);
     }
 
     /**
@@ -38,50 +34,46 @@ public class PessoaFisica extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btGrupoTipoPessoa = new javax.swing.ButtonGroup();
-        lbNome = new javax.swing.JLabel();
-        lbDataNasc = new javax.swing.JLabel();
-        lbCPF = new javax.swing.JLabel();
-        lbRG = new javax.swing.JLabel();
         lbTelefone = new javax.swing.JLabel();
+        txtEmail = new javax.swing.JTextField();
         lbEmail = new javax.swing.JLabel();
+        txtComp = new javax.swing.JTextField();
         lbEnd = new javax.swing.JLabel();
+        txtNum = new javax.swing.JFormattedTextField();
         lbComp = new javax.swing.JLabel();
         lbNum = new javax.swing.JLabel();
         txtNome = new javax.swing.JTextField();
         txtDataNasc = new javax.swing.JFormattedTextField();
+        lbNome = new javax.swing.JLabel();
         txtRG = new javax.swing.JFormattedTextField();
+        lbDataNasc = new javax.swing.JLabel();
         txtCPF = new javax.swing.JFormattedTextField();
+        lbCPF = new javax.swing.JLabel();
         txtTelefone = new javax.swing.JFormattedTextField();
+        lbRG = new javax.swing.JLabel();
         txtEnd = new javax.swing.JTextField();
-        txtEmail = new javax.swing.JTextField();
-        txtComp = new javax.swing.JTextField();
-        txtNum = new javax.swing.JFormattedTextField();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(219, 130, 40));
-        setForeground(java.awt.Color.red);
-
-        lbNome.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        lbNome.setText("Nome:");
-
-        lbDataNasc.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        lbDataNasc.setText("Data de Nascimento:");
-
-        lbCPF.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        lbCPF.setText("CPF:");
-
-        lbRG.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        lbRG.setLabelFor(lbRG);
-        lbRG.setText("RG:");
-        lbRG.setAlignmentX(0.5F);
+        setPreferredSize(new java.awt.Dimension(595, 325));
 
         lbTelefone.setText("Telefone:");
+
+        txtEmail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEmailActionPerformed(evt);
+            }
+        });
 
         lbEmail.setText("Email:");
 
         lbEnd.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbEnd.setText("Endereço:");
+
+        try {
+            txtNum.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("####")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+        txtNum.setText("");
 
         lbComp.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbComp.setText("Complemento:");
@@ -89,8 +81,9 @@ public class PessoaFisica extends javax.swing.JFrame {
         lbNum.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         lbNum.setText("Número:");
 
-        txtNome.setBackground(new java.awt.Color(214, 142, 71));
+        txtNome.setText("ALINE MARIA");
         txtNome.setToolTipText("");
+        txtNome.setName(""); // NOI18N
         txtNome.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtNomeActionPerformed(evt);
@@ -104,12 +97,18 @@ public class PessoaFisica extends javax.swing.JFrame {
         }
         txtDataNasc.setText("");
 
+        lbNome.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lbNome.setText("Nome:");
+
         try {
             txtRG.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##.###.###-#")));
         } catch (java.text.ParseException ex) {
             ex.printStackTrace();
         }
         txtRG.setText("");
+
+        lbDataNasc.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lbDataNasc.setText("Data de Nascimento:");
 
         try {
             txtCPF.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("###.###.###-##")));
@@ -124,6 +123,9 @@ public class PessoaFisica extends javax.swing.JFrame {
             }
         });
 
+        lbCPF.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lbCPF.setText("CPF:");
+
         try {
             txtTelefone.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("(##) ####-####")));
         } catch (java.text.ParseException ex) {
@@ -131,27 +133,18 @@ public class PessoaFisica extends javax.swing.JFrame {
         }
         txtTelefone.setText("");
 
+        lbRG.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        lbRG.setText("RG:");
+        lbRG.setAlignmentX(0.5F);
+
         txtEnd.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtEndActionPerformed(evt);
             }
         });
 
-        txtEmail.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtEmailActionPerformed(evt);
-            }
-        });
-
-        try {
-            txtNum.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("####")));
-        } catch (java.text.ParseException ex) {
-            ex.printStackTrace();
-        }
-        txtNum.setText("");
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
@@ -182,7 +175,7 @@ public class PessoaFisica extends javax.swing.JFrame {
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addComponent(lbNum)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(txtNum, javax.swing.GroupLayout.DEFAULT_SIZE, 57, Short.MAX_VALUE))))
+                                .addComponent(txtNum, javax.swing.GroupLayout.DEFAULT_SIZE, 61, Short.MAX_VALUE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(lbNome)
@@ -233,9 +226,11 @@ public class PessoaFisica extends javax.swing.JFrame {
                     .addComponent(txtComp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
-
-        pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void txtEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEmailActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEmailActionPerformed
 
     private void txtNomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNomeActionPerformed
         // TODO add your handling code here:
@@ -249,52 +244,8 @@ public class PessoaFisica extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_txtEndActionPerformed
 
-    private void txtEmailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEmailActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtEmailActionPerformed
-
-    /**
-     * @param ds
-     */
-    public static void main(Datastore ds) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PessoaFisica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PessoaFisica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PessoaFisica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PessoaFisica.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(() -> {
-            new PessoaFisica(ds).setVisible(true);
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.ButtonGroup btGrupoTipoPessoa;
     private javax.swing.JLabel lbCPF;
     private javax.swing.JLabel lbComp;
     private javax.swing.JLabel lbDataNasc;
