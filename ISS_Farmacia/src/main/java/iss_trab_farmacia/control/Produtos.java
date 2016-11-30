ab0dc1179ddb2353c48fe7f4d5c3e1a618595227
guest-iwn7E7 <guest-iwn7E7@DINSL009-099997>
2016-11-30 16:07:56 -0200

    Metodos de busca

--- ISS_Farmacia/src/main/java/iss_trab_farmacia/control/Produtos.java
+++ ISS_Farmacia/src/main/java/iss_trab_farmacia/control/Produtos.java
@@ -7,9 +7,11 @@ package iss_trab_farmacia.control;
 
 import iss_trab_farmacia.entity.Produto;
 import iss_trab_farmacia.util.ItemCompra;
+import java.util.List;
 import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.dao.BasicDAO;
+import org.mongodb.morphia.query.Query;
 
 /**
  *
@@ -40,5 +42,17 @@ public class Produtos extends BasicDAO<Produto, ObjectId>{
     
     public float sugestaoValor(Produto produto) {
         return (float) (this.custo(produto) * 1.5);
+    }
+    
+    public List<Produto> buscarMarca(String marca) {
+        Query<Produto> q = this.getDs().createQuery(Produto.class).field("marca").containsIgnoreCase(marca);
+        
+        return q.asList();
+    }
+    
+    public List<Produto> buscarDescricao(String descricao) {
+        Query<Produto> q = this.getDs().createQuery(Produto.class).field("descricao").containsIgnoreCase(descricao);
+        
+        return q.asList();
     }
 }
