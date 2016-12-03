4228d9148cfeae99759f318e0d82d15b9d0cf7cd
GChicha <guilherme.chichanoski@gmail.com>
2016-12-03 16:13:58 -0200

    Main cleanup

--- ISS_Farmacia/src/main/java/iss_trab_farmacia/ISS_TRAB_Farmacia.java
+++ ISS_Farmacia/src/main/java/iss_trab_farmacia/ISS_TRAB_Farmacia.java
@@ -5,11 +5,6 @@
  */
 package iss_trab_farmacia;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import org.mongodb.morphia.Datastore;
-import org.mongodb.morphia.Morphia;
-
 /**
  *
  * @author guilherme
@@ -20,18 +15,6 @@ public class ISS_TRAB_Farmacia {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        final Morphia morphia = new Morphia();
-        
-        morphia.mapPackage("iss_trab_farmacia.entity");
-        morphia.mapPackage("iss_trab_farmacia.util");
-        
-        final Datastore datastore;
-        datastore = morphia.createDatastore(new MongoClient(new MongoClientURI("mongodb://teste:123456@ds159737.mlab.com:59737/farmacia")),"farmacia");
-        
-        datastore.ensureIndexes();
-        
-        Login login = new Login();
-        login.setVisible(true);
     }
     
 }
