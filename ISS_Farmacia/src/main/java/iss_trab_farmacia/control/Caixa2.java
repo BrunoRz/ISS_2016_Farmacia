0b962225f487c9c44a9a0aca87e515654d0f3054
guest-NUM8iy <guest-NUM8iy@DINSL010-109170>
2017-01-16 22:40:29 -0200

    Caixa2 by swiss prince

---
+++ ISS_Farmacia/src/main/java/iss_trab_farmacia/control/Caixa2.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iss_trab_farmacia.control;
+
+import iss_trab_farmacia.entity.Caixa;
+import iss_trab_farmacia.util.SingletonBd;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.dao.BasicDAO;
+
+/**
+ *
+ * @author swiss_prince
+ */
+public class Caixa2 extends BasicDAO<Caixa, ObjectId>{
+
+    public Caixa2() {
+        super(Caixa.class, SingletonBd.getInstance().getDs());
+    }
+    
+    
+    public List<Caixa> buscarTodos(){
+        return this.createQuery().asList();
+    }
+    
+    public List<Caixa> buscarEntrada() {
+        return this.createQuery().field("entrada").equal(true).asList();
+    }
+    
+    public List<Caixa> buscarSaida(){
+        return this.createQuery().field("entrada").equal(false).asList();
+    }
+    
+    public double getSaldo() {
+        double soma = 0;
+        List<Caixa> lC = this.buscarTodos();
+        Iterator<Caixa> iC = lC.iterator();
+        
+        while(iC.hasNext()) {
+            Caixa caixa = iC.next();
+            if (caixa.isEntrada()) {
+                soma += caixa.getValor();
+            } 
+            else soma -= caixa.getValor();
+        }
+        
+        return soma;
+    }
+}
---
+++ ISS_Farmacia/src/main/java/iss_trab_farmacia/entity/Caixa.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iss_trab_farmacia.entity;
+
+import java.util.Date;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+/**
+ *
+ * @author guest-NUM8iy
+ */
+
+@Entity("caixa")
+public class Caixa {
+    @Id
+    private ObjectId id;
+    
+    private Date data = new Date();
+    
+    private boolean entrada;
+   
+    private double valor;
+
+    
+    public Caixa(boolean entrada, double valor) {
+        this.entrada = entrada;
+        this.valor = valor;
+    }
+    
+    public Caixa(Venda venda){
+        this.valor = venda.getTotal();
+        this.entrada = true;
+    }
+    
+    public Caixa(Compra compra){
+        this.valor = compra.getTotal();
+        this.entrada = false;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public boolean isEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(boolean entrada) {
+        this.entrada = entrada;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
